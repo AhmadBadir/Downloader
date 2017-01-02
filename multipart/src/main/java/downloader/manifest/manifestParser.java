ad528e2d920b7689f4c6b689ee413c8e238e5643
abadir <abadir@ABADIR-L0LB0.cisco.com>
2017-01-02 21:26:52 +0200

    added manifest Parser

---
+++ multipart/src/main/java/downloader/manifest/manifestParser.java
@@ -0,0 +1,24 @@
+package downloader.manifest;
+
+import java.io.IOException;
+
+public class manifestParser {
+
+	private String manifestFile = null;
+	
+    /**
+     * Parser constructor 
+     */
+	public manifestParser (String manifestFile) {
+		this.manifestFile = manifestFile;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	private boolean isValidUrl(String ManifestLine)  {
+		return false;
+	}
+	
+}
